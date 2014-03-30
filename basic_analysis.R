97a0ec81cfb14ac465e7cc141348285ac3d92724
Edgaras Dunajevas <edgaras.dunajevas@adform.com>
2014-03-31 00:21:06 +0300

    added few comments about utf characters in regex and dictionary

--- basic_analysis.R
+++ basic_analysis.R
@@ -3,7 +3,7 @@ library(stringr)
 library(tm)
 library(glmnet)
 d <- read.csv("data/comments.csv", stringsAsFactors=F, encoding="UTF-8")
-
+#intToUtf8 some problems with different utf quotes
 some.wild.characters <- 
   paste(c(enc2utf8("[[:punct:]]|[[:cntrl:]]|[[:digit:]]"),
         strsplit(intToUtf8(8210:8230),"")[[1]]),collapse="|")
@@ -24,6 +24,7 @@ write.csv(sort(unique(unlist(lapply(cp, strsplit, " ")))),
           file="dictionary.csv",
           row.names=F, fileEncoding="UTF-8")
 dtm <- DocumentTermMatrix(cp)
+#russian letters appear in dictionary, but it is not huge problem
 colnames(dtm)[1:100]
 dtm <- removeSparseTerms(DocumentTermMatrix(cp), 0.99)
 grep(intToUtf8(8221), colnames(dtm)[1:100])
